d201647254f0df0445c55a3d8bc3a441c8ceb562
Fozilkhon <buzrukxojayevfozilxon2007@gmail.com>
2022-04-07 05:31:53 -0700

    fixed 1

--- src/component/SignUp/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
-
-export default SignUp
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Navbar from "../../component/Navbar";
-import SignUp from "../../component/SignUp";
 
 export const Home = () => {
   return (
     <div>
-      <SignUp/>
       <Navbar />
     </div>
   );
